3d3d46c1cfd1830f7279fdf029de84447ee98161
24k-zilxh <abhiram.vadali@gmail.com>
2025-01-05 10:29:40 -0500

    program completed, final commit

--- searchsolve.py
+++ searchsolve.py
@@ -33,46 +33,46 @@ def get_2d_coords(lst,tgt):#lst is the search, tgt is the target as a string
      
 startpoints = get_2d_coords(search, fs)
 
-def up(tup): # this works 
-    if tup[0]-1<0: return None
-    try: return search[tup[0]-1][tup[1]]
+def up(tup,i): # this works, i is for loop iterator so we know how much to push off  
+    if tup[0]-i<0: return None
+    try: return search[tup[0]-i][tup[1]]
     except IndexError: return None  
 
-def down(tup): # this works
-    if tup[0]+1<0: return None
-    try: return search[tup[0]+1][tup[1]]  
+def down(tup,i): # this works
+    if tup[0]+i<0: return None
+    try: return search[tup[0]+i][tup[1]]  
     except IndexError: return None
-def left(tup): # this works
-    if tup[0]-1<0: return None
-    try: return search[tup[0]][tup[1]-1]
-    except IndexError: return None
-
-def right(tup): # this works
-    try: return search[tup[0]][tup[1]+1]  
+def left(tup,i): # this works
+    if tup[0]-i<0: return None
+    try: return search[tup[0]][tup[1]-i]
     except IndexError: return None
 
-def updiagl(tup): #this works
-    if tup[1]-1 <0: return None
-    if tup[0]-1 <0: return None
-    try: return search[tup[0]-1][tup[1]-1]
+def right(tup,i): # this works
+    try: return search[tup[0]][tup[1]+i]  
     except IndexError: return None
 
-def updiagr(tup): # this works
-    if tup[1]+1 <0: return None
-    if tup[0]-1 <0: return None
-    try: return search[tup[0]-1][tup[1]+1]
+def updiagl(tup,i): #this works
+    if tup[1]-i <0: return None
+    if tup[0]-i <0: return None
+    try: return search[tup[0]-i][tup[1]-i]
     except IndexError: return None
 
-def downdiagr(tup): # this works
-    if tup[1]+1 <0: return None
-    if tup[0]+1 <0: return None
-    try: return search[tup[0]+1][tup[1]+1]
+def updiagr(tup,i): # this works
+    if tup[1]+i <0: return None
+    if tup[0]-i <0: return None
+    try: return search[tup[0]-i][tup[1]+i]
     except IndexError: return None
 
-def downdiagl(tup): # this works
-    if tup[1]+1 <0: return None
-    if tup[0]-1 <0: return None
-    try: return search[tup[0]+1][tup[1]-1]
+def downdiagr(tup,i): # this works
+    if tup[1]+i <0: return None
+    if tup[0]+i <0: return None
+    try: return search[tup[0]+i][tup[1]+i]
+    except IndexError: return None
+
+def downdiagl(tup,i): # this works
+    if tup[1]+i <0: return None
+    if tup[0]-i <0: return None
+    try: return search[tup[0]+i][tup[1]-i]
     except IndexError: return None
 
 
@@ -82,59 +82,54 @@ for x in startpoints: # iterating through each starting point, getting the first
     for _ in trails:
         _.append(fs)
         
-    for y in range(0,1): # This function takes a step in each direction for the first 2 letters, then we know what direction to go in.
-        trails[0].append(up(x))
-        trails[1].append(down(x))
-        trails[2].append(left(x))
-        trails[3].append(right(x))
-        trails[4].append(updiagl(x))
-        trails[5].append(updiagr(x))
-        trails[6].append(downdiagl(x))
-        trails[7].append(downdiagr(x))
+    for y in range(1,len(query)): # This function takes a step in each direction for the first 2 letters, then we know what direction to go in.
+        trails[0].append(up(x,y)) # x,y is NOT The coordinates, X is the tuple and Y is the iterator
+        trails[1].append(down(x,y))
+        trails[2].append(left(x,y))
+        trails[3].append(right(x,y))
+        trails[4].append(updiagl(x,y))
+        trails[5].append(updiagr(x,y))
+        trails[6].append(downdiagl(x,y))
+        trails[7].append(downdiagr(x,y))
     
     for p in trails:
-        print(f"is {p} equal to {query[:2]}")
-        
-        if p == query[:2]:
+                
+        if p == query:
             
             dir = trails.index(p) #numerical direction in which the algorithim shall traverse in
             if dir==0:
                 dir="up"
                 lst=trails[0]
-
                 
             elif dir==1:
                 dir='down'
                 lst=trails[1]
-      
-                    
+                          
             elif dir==2:
                 dir='left'
                 lst=trails[2]
                 
-                    
             elif dir==3:
                 dir='right'
                 lst=trails[3]
               
             elif dir==4:
-                dir='updiagl'
+                dir='top left'
                 lst=trails[4]
           
             elif dir==5:
-                dir= 'updiagr'
+                dir= 'top right'
                 lst=trails[5]
            
             elif dir==6:
-                dir= 'downdiagl'
+                dir= 'bottom left'
                 lst=trails[6]
         
             elif dir==7:
-                dir= 'downdiagr'
+                dir= 'bottom right'
                 lst=trails[7]
             
-                print(lst)
-            print(dir)            
+            print(f'Start from coordinate {x} going {dir}')            
         
         found=False    
         
